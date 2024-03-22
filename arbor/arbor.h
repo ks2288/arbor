1e6513b877c60ee198c5cdbc881cb7962cfcd453
Kenny Sofranko <kmsofranko@gmail.com>
2024-03-21 20:08:49 -0400

    initial

---
+++ arbor/arbor.h
@@ -0,0 +1,18 @@
+//
+//  arbor.h
+//  arbor
+//
+//  Created by Kenny Sofranko on 3/21/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for arbor.
+FOUNDATION_EXPORT double arborVersionNumber;
+
+//! Project version string for arbor.
+FOUNDATION_EXPORT const unsigned char arborVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <arbor/PublicHeader.h>
+
+
